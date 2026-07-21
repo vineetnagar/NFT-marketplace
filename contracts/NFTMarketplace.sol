340a39433b12bb0c748d07a2f029b95cc1d6bd14
Vineet Nagar <dhakadvineet71@gmail.com>
2026-07-21 16:20:35 +0530

    created the struct of NFT and function to charge initial price to create NFT

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -4,3 +4,38 @@ pragma solidity ^0.8.35;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemSold;
+
+    address payable owner;
+
+    
+    mapping(uint => MarketItem) private idMarketItem;
+
+    struct MarketItem{
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    event idMarketItemCreated{
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    };
+
+    constructor() ERC721("NFT Metavarse Token", "MYNFT") {
+        owner = payable(msg.sender);
+    }
+
+}
