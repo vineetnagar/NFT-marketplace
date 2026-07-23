2e18980cb00ef230a779c3c177ce0482b7ecc2be
Vineet Nagar <dhakadvineet71@gmail.com>
2026-07-23 22:43:32 +0530

    created the token, item creating and reselling item function

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -53,4 +53,55 @@ contract NFTMarketplace is ERC721URIStorage {
     function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
+
+    //Creating the token
+
+    function createToken(string memory tokenURI, uint256 price) public payable{
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        createMarketItem(newTOkenId, price);
+        return newTOkenId;
+    }
+
+    //Creating the MarketItem
+
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+        require(price > 0, "price must be atleast 1");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+        idMarketItem[tokenid] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+        _transfer(msg.sender, address(this), tokenId);
+        emit idMarketItemCreated(
+            tokenId,
+            msg.sender,
+            address(this),
+            price,
+            false
+        );
+    };
+
+    //Function for resellig token
+
+    function reSellToken(uint256 price, uint256 tokenId) public payable{
+        require(idMarketItem[tokenId].owner == msg.sender, "Only ITem owner can resell token");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+        idMarketItem[tokenId].sold = false;
+        idMarketItem[tokenId].price = price;
+        idMarketItem[tokenId].seller = payable(msg.sender);
+        idMarketItem[tokenId].owner = address(this);
+
+        _itemSold.decreament();
+        _transfer(msg.sender, address(this), tokenId);
+        }
 }
