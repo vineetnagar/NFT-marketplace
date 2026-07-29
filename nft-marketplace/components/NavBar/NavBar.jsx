9933bcd3c33dd0b50c9a082da60cff2def632831
Vineet Nagar <dhakadvineet71@gmail.com>
2026-07-29 22:49:48 +0530

    creating the navbar page in react

--- nft-marketplace/components/NavBar/NavBar.jsx
+++ nft-marketplace/components/NavBar/NavBar.jsx
@@ -1,9 +1,127 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { MdNotifications } from "react-icons/md";
+import { BsSearch } from "react-icons/bs";
+import { CgMenuLeft, CgMenuRight } from "react-icons/cg";
 
 import Style from "./NavBar.module.css";
+import { Discover, HelpCenter, Notification, Profile, SideBar } from "./index";
+import { Button } from "./componentIndex";
+import images from "../../img";
 
 const NavBar = () => {
-  return <div>Footer</div>;
+  //UseState
+  const [discover, setDiscover] = useState(false);
+  const [help, setHelp] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  const openMenu = (e) => {
+    const btnText = e.target.innerText;
+    if (btnText == "Discover") {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    } else if ((btnText = "Help Center")) {
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(false);
+    } else {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+  };
+
+  const openNotification = () => {
+    if (!notification) {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(true);
+      setProfile(false);
+    } else {
+      setNotification(false);
+    }
+  };
+  return (
+    <div className={Style.navbar}>
+      <div className={Style.navbar_container}>
+        //Left Section
+        <div className={Style.navbar_container_left}>
+          <div className={Style.logo}>
+            <Image
+              src={images.logo}
+              alt="NFT MARKET PLACE"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={Style.navbar_container_left_box_input}></div>
+          <div className={Style.navbar_container_left_box_input_box}>
+            <input type="text" placeholder="Search NFT" />
+            <BsSearch onClick={() => {}} className={Style.search_icon} />
+          </div>
+        </div>
+        //Right Section
+        <div className={Style.navbar_container_right}>
+          <div classname={Style.navbar_container_right_discover}>
+            //Discover Menu
+            <p onClick={(e) => openMenu(e)}>Discover</p>
+            {discover && (
+              <div classname={Style.navbar_container_right_discover_box}>
+                <Discover />
+              </div>
+            )}
+          </div>
+          //Help Center Menu
+          <div classname={Style.navbar_container_right - help}>
+            <p onClick={(e) => openMenu(e)}>Help Center</p>
+            {help && (
+              <div classname={Style.navbar_container_right_help_box}></div>
+            )}
+          </div>
+          //Notification Center Menu
+          <div classname={Style.navbar_container_right_notify}>
+            <MdNotifications
+              classname={Style.notify}
+              onClick={() => openNotification()}
+            />
+            {notification && <Notification />}
+          </div>
+          //Create Button Section
+          <div classname={Style.navbar_container_right_button}>
+            <Button btnText="Create" />
+          </div>
+          //User Profile
+          <div classname={Style.navbar_container_right_profile_box}></div>
+          <div classname={Style.navbar_container_right_profile}>
+            <Image
+              src={images.user1}
+              alt="Profile"
+              width={40}
+              height={40}
+              onClick={() => openProfile()}
+              classname={Style.navbar_container_right_profile}
+            />
+
+            {profile && <Profile />}
+          </div>
+          //Menu Button
+          <div classname={Style.navbar_container_right_menuBtn}>
+            <CgMenuRight
+              classname={Style.menuIcon}
+              onClick={() => openSideBar()}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- nft-marketplace/components/NavBar/index.js
+++ nft-marketplace/components/NavBar/index.js
@@ -1,1 +1,7 @@
 import Discover from "./Discover/Discover";
+import HelpCenter from "./Discover/HelpCenter/HelpCenter";
+import Profile from "./Profile/Profile";
+import Notification from "./Notification/Notification";
+import SideBar from "./SideBar/SideBar";
+
+export { Discover, HelpCenter, Profile, Notification, SideBar };
