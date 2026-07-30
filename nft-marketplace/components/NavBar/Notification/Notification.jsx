ee01eecd4ee31f5586e59f76700420dbede65867
Vineet Nagar <dhakadvineet71@gmail.com>
2026-07-31 00:29:38 +0530

    completed the notification compone t dropdown

--- nft-marketplace/components/NavBar/Notification/Notification.jsx
+++ nft-marketplace/components/NavBar/Notification/Notification.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-
+import Image from "next/image";
 import Style from "./Notification.module.css";
+import images from "../../../img";
 
 const Notification = () => {
-  return <div>Notification</div>;
+  return (
+    <div className={Style.notification}>
+      <p>Notification</p>
+      <div className={Style.notification_box}>
+        <div className={Style.notification_box_img}>
+          <Image
+            src={images.user1}
+            alt="profile image"
+            width={50}
+            height={50}
+            className={Style.notification_box_img}
+          />
+        </div>
+        <div className={Style.notification_box_info}>
+          <h4>Mohit Singh</h4>
+          <p>Measure action of user...</p>
+          <small>3 minutes ago</small>
+        </div>
+        <span className={Style.notification_box_new}></span>
+      </div>
+    </div>
+  );
 };
 
 export default Notification;
