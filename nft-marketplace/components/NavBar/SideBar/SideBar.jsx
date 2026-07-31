2aee8de537ccf9653fbe512ef6bd865003acd528
Vineet Nagar <dhakadvineet71@gmail.com>
2026-07-31 22:27:38 +0530

    completed the structure of whole navbar components

---
+++ nft-marketplace/components/NavBar/SideBar/SideBar.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { GrClose } from "react-icons/gr";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialInstagram,
+  TiArrowSortedDown,
+  TiArrowSortedUp,
+} from "react-icons/ti";
+import Style from "./SideBar.module.css";
+import images from "../../../img";
+import Button from "../../Button/Button";
+
+const SideBar = ({ setOpenSideMenu }) => {
+  const [openDiscover, setOpenDiscover] = useState(false);
+  const [openHelp, setOpenHelp] = useState(false);
+  const discover = [
+    { name: "Collection", link: "collection" },
+    { name: "Search", link: "search" },
+    { name: "Author Profile", link: "author-profile" },
+    { name: "NFT Details", link: "NFT-details" },
+    { name: "Account Setting", link: "account-setting" },
+    { name: "Connect Wallet", link: "connect-wallet" },
+    { name: "Blog", link: "blog" },
+  ];
+
+  const openDiscoverMenu = () => {
+    if (!openDiscover) {
+      setOpenDiscover(true);
+    } else {
+      setOpenDiscover(false);
+    }
+  };
+  const openHelpMenu = () => {
+    if (!openHelp) {
+      setOpenHelp(true);
+    } else {
+      setOpenHelp(false);
+    }
+  };
+
+  const closeSideBar = () => {
+    setOpenSideMenu(false);
+  };
+  return (
+    <div className={Style.SideBar}>
+      <GrClose
+        className={Style.SideBar_closeBtn}
+        onClick={() => closeSideBar()}
+      />
+
+      <div className={Style.SideBar_box}>
+        <Image src={images.logo} alt="logo" width={150} height={150} />
+        <p>
+          Discover the most outstanding articles on all the topics of NFT's and
+          your own stories and share them.{" "}
+        </p>
+        <div className={Style.sideBar_social}>
+          <a href="#">
+            <TiSocialFacebook />
+          </a>
+          <a href="#">
+            <TiSocialLinkedin />
+          </a>
+          <a href="#">
+            <TiSocialTwitter />
+          </a>
+          <a href="#">
+            <TiSocialYoutube />
+          </a>
+          <a href="#">
+            <TiSocialInstagram />
+          </a>
+        </div>
+      </div>
+      <div className={Style.SideBar_menu}>
+        <div>
+          <div
+            className={Style.SideBar_menu_box}
+            onClick={() => openDiscoverMenu()}
+          >
+            <p>Discover</p>
+            <TiArrowSortedDown />
+          </div>
+
+          {openDiscover && (
+            <div className={Style.SideBar_discover}>
+              {discover.map((el, i) => (
+                <p key={i + 1}>
+                  <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
+                </p>
+              ))}
+            </div>
+          )}
+        </div>
+
+        <div>
+          <div
+            className={Style.SideBar_menu_box}
+            onClick={() => openHelpMenu()}
+          >
+            <p>Help Center</p>
+            <TiArrowSortedDown />
+          </div>
+
+          {openHelp && (
+            <div className={Style.sideBar_discover}>
+              {helpCenter.map((el, i) => (
+                <p key={i + 1}>
+                  <Link href={{ pathname: `${el.Link}` }}>{el.name}</Link>
+                </p>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className={Style.sideBar_button}>
+        <Button btnName="Create"></Button>
+        <Button btnName="Connect wallet"></Button>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
