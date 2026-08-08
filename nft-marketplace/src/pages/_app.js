5f2ce2ded0a04c45736ec5e19383498964712875
Vineet Nagar <dhakadvineet71@gmail.com>
2026-08-08 21:43:56 +0530

    created the structure of footer component

--- nft-marketplace/src/pages/_app.js
+++ nft-marketplace/src/pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
 
 const App = ({ Component, pageProps }) => (
   <div>
     <NavBar />
     <Component {...pageProps} />;
+    <Footer />
   </div>
 );
 
