9d7bc28cec8dd34d0d84430b8ba42408c3920d09
Vineet Nagar <dhakadvineet71@gmail.com>
2026-08-08 23:29:31 +0530

    created the css of HeroSection.jsx

--- nft-marketplace/src/pages/index.js
+++ nft-marketplace/src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Style from "../styles/index.module.css";
+import { HeroSection } from "../../components/NavBar/componentIndex";
 
 const index = () => {
-  return <div>Home</div>;
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+    </div>
+  );
 };
 
 export default index;
