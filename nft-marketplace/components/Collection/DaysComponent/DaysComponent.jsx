e1b687ea2d81c20be6b2c1d7d4a2858bccc70bba
Vineet Nagar <dhakadvineet71@gmail.com>
2026-08-14 14:10:42 +0530

    completed the day component structure

--- nft-marketplace/components/Collection/DaysComponent/DaysComponent.jsx
+++ nft-marketplace/components/Collection/DaysComponent/DaysComponent.jsx
@@ -1,8 +1,82 @@
 import React from "react";
 import Style from "./DaysComponent.module.css";
+import images from "../../../img";
+import Image from "next/image";
+import { MdVerified } from "react-icons/md";
 
 const DaysComponent = () => {
-  return <div>Component</div>;
+  return (
+    <div className={Style.daysComponent}>
+      <div className={Style.daysComponent_box}>
+        <div classNam={Style.daysComponent_box_img}>
+          <Image
+            src={images.creatorbackground1}
+            alt="Profile background"
+            width={500}
+            height={300}
+            className={Style.daysComponent_box_img_img}
+            objectFit="covers"
+          />
+        </div>
+
+        <div className={Style.daysComponent_box_profile}>
+          <Image
+            src={images.creatorbackground2}
+            alt="profile"
+            width={200}
+            height={200}
+            className={Style.daysComponent_box_img_1}
+            objectFit="covers"
+          />
+          <Image
+            src={images.creatorbackground2}
+            alt="profile"
+            width={200}
+            height={200}
+            className={Style.daysComponent_box_img_2}
+            objectFit="covers"
+          />
+          <Image
+            src={images.creatorbackground2}
+            alt="profile"
+            width={200}
+            height={200}
+            className={Style.daysComponent_box_img_3}
+            objectFit="covers"
+          />
+        </div>
+
+        <div className={Style.daysComponent_box_title}>
+          <h2>Amazing collection</h2>
+          <div className={Style.daysComponent_box_title_info}>
+            <div className={Style.daysComponent_box_title_info_profile}>
+              <Image
+                src={images.user1}
+                alt="profile"
+                width={30}
+                height={30}
+                objectFit="covers"
+                className={Style.daysComponent_box_title_info_profile_img}
+              />
+              <p>
+                Creator{" "}
+                <span>
+                  Vineet Nagar{" "}
+                  <small>
+                    <MdVerified />
+                  </small>
+                </span>
+              </p>
+            </div>
+
+            <div className={Style.daysComponent_box_title_info_profile}>
+              <small>1.255 ETH</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DaysComponent;
