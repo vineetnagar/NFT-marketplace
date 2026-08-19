d1f98b2a156920253f0fbaebc8c2a9131dcb2748
Vineet Nagar <dhakadvineet71@gmail.com>
2026-08-20 00:55:10 +0530

    completed connect wallet page

--- nft-marketplace/img/index.js
+++ nft-marketplace/img/index.js
@@ -43,6 +43,10 @@ import musiceWave from "./musicwave.png";
 import update from "./update.png";
 import earn from "./eran.png";
 import NFTVideo from "./nftvideo.png";
+import provider1 from "./provider-1.png";
+import provider2 from "./provider-2.png";
+import provider3 from "./provider-3.png";
+import provider4 from "./provider-4.png";
 
 export default {
   collection,
@@ -90,4 +94,8 @@ export default {
   zgcmq,
   earn,
   NFTVideo,
+  provider1,
+  provider2,
+  provider3,
+  provider4,
 };
