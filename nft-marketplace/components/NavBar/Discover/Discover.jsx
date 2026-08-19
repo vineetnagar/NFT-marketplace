4a10cc9e2d814e68f3d81fefc97b3d62be6e4818
Vineet Nagar <dhakadvineet71@gmail.com>
2026-08-19 23:40:17 +0530

    completed NFT uploading NFT page

--- nft-marketplace/components/NavBar/Discover/Discover.jsx
+++ nft-marketplace/components/NavBar/Discover/Discover.jsx
@@ -26,6 +26,10 @@ const Discover = () => {
       link: "account",
     },
     {
+      name: "Upload NFT",
+      link: "uploadNFT",
+    },
+    {
       name: "Connect Wallet",
       link: "connect-wallet",
     },
