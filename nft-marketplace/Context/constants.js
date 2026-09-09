4f0232a46b559190c780b6aace1524393ac44733
Vineet Nagar <dhakadvineet71@gmail.com>
2026-09-09 12:15:51 +0530

    fixed the bugs

--- nft-marketplace/Context/constants.js
+++ nft-marketplace/Context/constants.js
@@ -1,7 +1,7 @@
-// 0x5FbDB2315678afecb367f032d93F642f64180aa3
+// 0x3851AF5538af01B12431a56076B89Df4835932DD
 
 import nftMarkeplace from "./NFTMarketplace.json";
 
 export const NFTMarketplaceAddress =
-  " 0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  "0x3851AF5538af01B12431a56076B89Df4835932DD";
 export const NFTMarketplaceABI = nftMarkeplace.abi;
